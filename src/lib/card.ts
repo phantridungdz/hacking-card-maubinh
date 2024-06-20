7f594f03b64b70741ca62161a5b737ae39abe37d
Phant Chase <56990218+phantridungdz@users.noreply.github.com>
2024-06-20 21:44:32 +0700

    update card type b52

--- src/lib/card.ts
+++ src/lib/card.ts
@@ -104,6 +104,59 @@ import queen_of_diamonds_2 from '../../assets/cards2/queen_of_diamonds.png';
 import queen_of_hearts_2 from '../../assets/cards2/queen_of_hearts.png';
 import queen_of_spades_2 from '../../assets/cards2/queen_of_spades.png';
 
+import ten_of_clubs_3 from '../../assets/cards3/10_of_clubs.png';
+import ten_of_diamonds_3 from '../../assets/cards3/10_of_diamonds.png';
+import ten_of_hearts_3 from '../../assets/cards3/10_of_hearts.png';
+import ten_of_spades_3 from '../../assets/cards3/10_of_spades.png';
+import two_of_clubs_3 from '../../assets/cards3/2_of_clubs.png';
+import two_of_diamonds_3 from '../../assets/cards3/2_of_diamonds.png';
+import two_of_hearts_3 from '../../assets/cards3/2_of_hearts.png';
+import two_of_spades_3 from '../../assets/cards3/2_of_spades.png';
+import three_of_clubs_3 from '../../assets/cards3/3_of_clubs.png';
+import three_of_diamonds_3 from '../../assets/cards3/3_of_diamonds.png';
+import three_of_hearts_3 from '../../assets/cards3/3_of_hearts.png';
+import three_of_spades_3 from '../../assets/cards3/3_of_spades.png';
+import four_of_clubs_3 from '../../assets/cards3/4_of_clubs.png';
+import four_of_diamonds_3 from '../../assets/cards3/4_of_diamonds.png';
+import four_of_hearts_3 from '../../assets/cards3/4_of_hearts.png';
+import four_of_spades_3 from '../../assets/cards3/4_of_spades.png';
+import five_of_clubs_3 from '../../assets/cards3/5_of_clubs.png';
+import five_of_diamonds_3 from '../../assets/cards3/5_of_diamonds.png';
+import five_of_hearts_3 from '../../assets/cards3/5_of_hearts.png';
+import five_of_spades_3 from '../../assets/cards3/5_of_spades.png';
+import six_of_clubs_3 from '../../assets/cards3/6_of_clubs.png';
+import six_of_diamonds_3 from '../../assets/cards3/6_of_diamonds.png';
+import six_of_hearts_3 from '../../assets/cards3/6_of_hearts.png';
+import six_of_spades_3 from '../../assets/cards3/6_of_spades.png';
+import seven_of_clubs_3 from '../../assets/cards3/7_of_clubs.png';
+import seven_of_diamonds_3 from '../../assets/cards3/7_of_diamonds.png';
+import seven_of_hearts_3 from '../../assets/cards3/7_of_hearts.png';
+import seven_of_spades_3 from '../../assets/cards3/7_of_spades.png';
+import eight_of_clubs_3 from '../../assets/cards3/8_of_clubs.png';
+import eight_of_diamonds_3 from '../../assets/cards3/8_of_diamonds.png';
+import eight_of_hearts_3 from '../../assets/cards3/8_of_hearts.png';
+import eight_of_spades_3 from '../../assets/cards3/8_of_spades.png';
+import nine_of_clubs_3 from '../../assets/cards3/9_of_clubs.png';
+import nine_of_diamonds_3 from '../../assets/cards3/9_of_diamonds.png';
+import nine_of_hearts_3 from '../../assets/cards3/9_of_hearts.png';
+import nine_of_spades_3 from '../../assets/cards3/9_of_spades.png';
+import ace_of_clubs_3 from '../../assets/cards3/ace_of_clubs.png';
+import ace_of_diamonds_3 from '../../assets/cards3/ace_of_diamonds.png';
+import ace_of_hearts_3 from '../../assets/cards3/ace_of_hearts.png';
+import ace_of_spades_3 from '../../assets/cards3/ace_of_spades.png';
+import jack_of_clubs_3 from '../../assets/cards3/jack_of_clubs.png';
+import jack_of_diamonds_3 from '../../assets/cards3/jack_of_diamonds.png';
+import jack_of_hearts_3 from '../../assets/cards3/jack_of_hearts.png';
+import jack_of_spades_3 from '../../assets/cards3/jack_of_spades.png';
+import king_of_clubs_3 from '../../assets/cards3/king_of_clubs.png';
+import king_of_diamonds_3 from '../../assets/cards3/king_of_diamonds.png';
+import king_of_hearts_3 from '../../assets/cards3/king_of_hearts.png';
+import king_of_spades_3 from '../../assets/cards3/king_of_spades.png';
+import queen_of_clubs_3 from '../../assets/cards3/queen_of_clubs.png';
+import queen_of_diamonds_3 from '../../assets/cards3/queen_of_diamonds.png';
+import queen_of_hearts_3 from '../../assets/cards3/queen_of_hearts.png';
+import queen_of_spades_3 from '../../assets/cards3/queen_of_spades.png';
+
 const cardImagesSet1: { [key: string]: string } = {
   '2_of_spades': two_of_spades,
   '2_of_clubs': two_of_clubs,
@@ -213,10 +266,65 @@ const cardImagesSet2: { [key: string]: string } = {
   ace_of_clubs: ace_of_clubs_2,
   ace_of_diamonds: ace_of_diamonds_2,
 };
+const cardImagesSet3: { [key: string]: string } = {
+  '2_of_spades': two_of_spades_3,
+  '2_of_clubs': two_of_clubs_3,
+  '2_of_diamonds': two_of_diamonds_3,
+  '2_of_hearts': two_of_hearts_3,
+  '3_of_hearts': three_of_hearts_3,
+  '3_of_spades': three_of_spades_3,
+  '3_of_clubs': three_of_clubs_3,
+  '3_of_diamonds': three_of_diamonds_3,
+  '4_of_spades': four_of_spades_3,
+  '4_of_hearts': four_of_hearts_3,
+  '4_of_clubs': four_of_clubs_3,
+  '4_of_diamonds': four_of_diamonds_3,
+  '5_of_spades': five_of_spades_3,
+  '5_of_hearts': five_of_hearts_3,
+  '5_of_clubs': five_of_clubs_3,
+  '5_of_diamonds': five_of_diamonds_3,
+  '6_of_spades': six_of_spades_3,
+  '6_of_hearts': six_of_hearts_3,
+  '6_of_clubs': six_of_clubs_3,
+  '6_of_diamonds': six_of_diamonds_3,
+  '7_of_spades': seven_of_spades_3,
+  '7_of_hearts': seven_of_hearts_3,
+  '7_of_clubs': seven_of_clubs_3,
+  '7_of_diamonds': seven_of_diamonds_3,
+  '8_of_spades': eight_of_spades_3,
+  '8_of_hearts': eight_of_hearts_3,
+  '8_of_clubs': eight_of_clubs_3,
+  '8_of_diamonds': eight_of_diamonds_3,
+  '9_of_spades': nine_of_spades_3,
+  '9_of_hearts': nine_of_hearts_3,
+  '9_of_clubs': nine_of_clubs_3,
+  '9_of_diamonds': nine_of_diamonds_3,
+  '10_of_spades': ten_of_spades_3,
+  '10_of_hearts': ten_of_hearts_3,
+  '10_of_clubs': ten_of_clubs_3,
+  '10_of_diamonds': ten_of_diamonds_3,
+  jack_of_spades: jack_of_spades_3,
+  jack_of_hearts: jack_of_hearts_3,
+  jack_of_clubs: jack_of_clubs_3,
+  jack_of_diamonds: jack_of_diamonds_3,
+  queen_of_spades: queen_of_spades_3,
+  queen_of_hearts: queen_of_hearts_3,
+  queen_of_clubs: queen_of_clubs_3,
+  queen_of_diamonds: queen_of_diamonds_3,
+  king_of_spades: king_of_spades_3,
+  king_of_hearts: king_of_hearts_3,
+  king_of_clubs: king_of_clubs_3,
+  king_of_diamonds: king_of_diamonds_3,
+  ace_of_spades: ace_of_spades_3,
+  ace_of_hearts: ace_of_hearts_3,
+  ace_of_clubs: ace_of_clubs_3,
+  ace_of_diamonds: ace_of_diamonds_3,
+};
 
 const cardSets = {
   set1: cardImagesSet1,
   set2: cardImagesSet2,
+  set3: cardImagesSet3,
 };
 
 type CardSet = 'set1' | 'set2';
--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,7 +1,7 @@
 export const roomTypes = [
   100, 500, 1000, 2000, 5000, 10000, 20000, 50000, 100000,
 ];
-export const cardTypes = ['set1', 'set2'];
+export const cardTypes = ['set1', 'set2', 'set3'];
 export const targetSites = ['RIK', 'HIT', 'B52'];
 export const fromHitSites = [
   'HIT',
